4e4bf5e59f31335f89ee381ee549ee1cd63cb746
Duan Yi <yumendy@163.com>
2014-09-01 03:20:28 +0800

    online

--- mstc_website/mstc_website/settings.py
+++ mstc_website/mstc_website/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'DjangoUeditor',
     'Publish',
 )
 
@@ -82,5 +81,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './static/'
 MEDIA_ROOT = './Publish/media/'
 MEDIA_URL = '/media/'
